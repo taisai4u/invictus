2ea5e955e16b793bfa28995f6098375b2ca742cf
notken12 <kenzhou084@gmail.com>
2026-03-10 12:14:05 -0400

    read data from arduino

---
+++ matlab/arduino_reading.m
@@ -0,0 +1,27 @@
+Fs = 100;
+samplesPerRead = 10;
+runTime = 20;
+isVerbose = false;
+
+if ~(exist('a', 'var') && isvalid(a))
+    a = arduino;
+end
+if ~exist('imu', 'var')
+    imu = bno055(a, 'SampleRate', Fs, 'OutputFormat', 'matrix', ...
+        'SamplesPerRead', samplesPerRead, 'I2CAddress', 0x28, 'OperatingMode','amg');
+end
+compFilt = complementaryFilter('SampleRate', Fs);
+
+tuner = HelperOrientationFilterTuner(compFilt);
+tic
+
+while toc <= runTime
+    [accel, gyro, mag, t, overrun] = imu();
+
+    if (isVerbose && overrun > 0)
+        fprintf('%d samples overrun ...\n', overrun);
+    end
+
+    q = compFilt(accel, gyro, mag);
+    update(tuner, q);
+end
